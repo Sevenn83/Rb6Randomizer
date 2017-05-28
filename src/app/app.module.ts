35fd8256307218f0ac3a610152c81cceab4af232
Asshritt <chouraki.hugo@gmail.com>
2017-05-28 22:36:30 +0200

    Souçaï traduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { GetOperatorService } from '../services/getOperator.service';
 import { HttpModule } from '@angular/http'
 
+import {Component} from '@angular/core';
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { Http } from '@angular/http';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -23,7 +28,14 @@ import { HttpModule } from '@angular/http'
   imports: [
     HttpModule,
     BrowserModule,
-    IonicModule.forRoot(MyApp, { tabsPlacement: 'top' })
+    IonicModule.forRoot(MyApp, { tabsPlacement: 'top' }),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [Http]
+      }
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -39,4 +51,20 @@ import { HttpModule } from '@angular/http'
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
+@Component({
+  selector: 'page-schedule',
+  templateUrl: 'schedule.html'
+})
 export class AppModule {}
+
+export function createTranslateLoader(http: Http) {
+  return new TranslateHttpLoader(http, './translate/', '.json');
+}
+
+export class SchedulePage {
+  constructor(translate: TranslateService) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
+}
