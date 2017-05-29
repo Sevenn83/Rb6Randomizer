bf84beb48fa187a4a363b827f886836046961864
Sevenn <thomas.s833@gmail.com>
2017-05-29 03:09:50 +0200

    fichier trouver et importer, mais le translate se fait pas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { GetOperatorService } from '../services/getOperator.service';
 import { HttpModule } from '@angular/http'
 
-import {Component} from '@angular/core';
 import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { Http } from '@angular/http';
@@ -35,7 +34,8 @@ import { Http } from '@angular/http';
         useFactory: (createTranslateLoader),
         deps: [Http]
       }
-    })
+    }),
+    TranslateModule.forChild()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -51,20 +51,14 @@ import { Http } from '@angular/http';
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-@Component({
-  selector: 'page-schedule',
-  templateUrl: 'schedule.html'
-})
-export class AppModule {}
 
-export function createTranslateLoader(http: Http) {
-  return new TranslateHttpLoader(http, './translate/', '.json');
+export class AppModule {
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use(navigator.language.split('-')[0]);
+  }
 }
 
-export class SchedulePage {
-  constructor(translate: TranslateService) {
-    // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('en');
-    translate.use('en');
-  }
+export function createTranslateLoader(http: Http) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
